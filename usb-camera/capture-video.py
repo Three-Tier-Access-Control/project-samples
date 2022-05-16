d026e723eaaa3f7e578cfc614230604965c07ede
Shumba Ashley <ashleytshumba@gmail.com>
2022-05-16 21:05:34 +0200

    detect face

--- usb-camera/capture-video.py
+++ usb-camera/capture-video.py
@@ -1,19 +1,25 @@
 
-# import the opencv library
+# import the opencv and  library
 import cv2
-  
+import face_recognition
   
 # define a video capture object
-vid = cv2.VideoCapture(4) # default webcam  0 , usb camera 4
+vid = cv2.VideoCapture(2) # default webcam  0 , usb camera 4
   
-while(True):
-      
-    # Capture the video frame
-    # by frame
+while(True):  
+    # Capture the video frame by frame
     ret, frame = vid.read()
-  
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_locations = face_recognition.face_locations(frame)
+
+    print(face_locations)
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in face_locations:
+        cv2.rectangle(frame, (h, x), (y, w), (0, 255, 0), 2)
+
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('Video', frame)
       
     # the 'q' button is set as the
     # quitting button you may use any
