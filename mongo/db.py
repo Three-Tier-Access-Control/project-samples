042c8ba2977e323b1083b6fd36db2f99ec56a716
Shumba Ashley <ashleytshumba@gmail.com>
2022-05-20 14:27:44 +0200

    inserting encodings in database

--- mongo/db.py
+++ mongo/db.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from pymongo import MongoClient
 import numpy as np
-
+import face_recognition
 
 client = MongoClient('localhost', 27017)
 db = client['face_db']
@@ -9,31 +9,56 @@ db = client['face_db']
 
 faces = db["face"]
 
+# Load a sample picture and learn how to recognize it.
+ashley_image = face_recognition.load_image_file(
+    "face-recognition/known_faces/Ashley_Shumba_0003.png")
+    
+ashley_face_encoding = face_recognition.face_encodings(ashley_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+sam_image = face_recognition.load_image_file(
+    "face-recognition/known_faces/Sam Phiri.jpg")
+sam_face_encoding = face_recognition.face_encodings(sam_image)[0]
+
+# Load a third sample picture and learn how to recognize it.
+volition_image = face_recognition.load_image_file(
+    "face-recognition/known_faces/Volition Mashamba.jpg")
+volition_face_encoding = face_recognition.face_encodings(volition_image)[0]
 
 
-first_person_name = "John"
-first_sample_face_embedding = np.random.rand(128).tolist() 
+first_person_name = "Ashley Shumba"
+first_sample_face_embedding = ashley_face_encoding.tolist()
 
-second_person_name = "Julia"
-second_sample_face_embedding = np.random.rand(128).tolist()
+second_person_name = "Sam Phiri"
+second_sample_face_embedding = sam_face_encoding.tolist()
 
-# faces.insert_many([
-#     {"name": first_person_name, "embedding": first_sample_face_embedding},
-#     {"name": second_person_name, "embedding": second_sample_face_embedding}
-# ])
+third_person_name = "Volition Mashamba"
+third_sample_face_embedding = volition_face_encoding.tolist()
+
+print("Encoding Complete....")
+
+faces.insert_many([
+    {"name": first_person_name, "embedding": first_sample_face_embedding},
+    {"name": second_person_name, "embedding": second_sample_face_embedding},
+    {"name": third_person_name, "embedding": third_sample_face_embedding}
+])
+
+
+print("Inserting Complete....")
 
 # faces.insert_one([
 #     {"name": first_person_name, "embedding": first_sample_face_embedding},
 # ])
 
-# 
+#
 # faces.delete_many({})
 
 # count documents
 # count = faces.count_documents({})
 
 all_docs = list(faces.find({}))
-names, embeddings = [doc["name"] for doc in all_docs], [doc["embedding"] for doc in all_docs]
+names, embeddings = [doc["name"]
+                     for doc in all_docs], [doc["embedding"] for doc in all_docs]
 
 embeddings = np.array(embeddings)
 
@@ -41,4 +66,3 @@ embeddings = np.array(embeddings)
 target_embedding = np.random.rand(128)
 pprint(names)
 pprint(embeddings)
-pprint(count)
