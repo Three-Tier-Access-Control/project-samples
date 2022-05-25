44749c201b46797567a87a86a89d205ea50873df
Shumba Ashley <ashleytshumba@gmail.com>
2022-05-25 21:43:35 +0200

    remote mongo database

--- mongo/insert_faces.py
+++ mongo/insert_faces.py
@@ -3,7 +3,10 @@ from pymongo import MongoClient
 import numpy as np
 import face_recognition
 
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
+
+client = MongoClient("mongodb+srv://ashleytshumba:02june1997@cluster0.ie2a2.mongodb.net/?retryWrites=true&w=majority")
+
 db = client['face_db']
 
 faces = db["face"]
