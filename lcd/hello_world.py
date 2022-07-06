e37184f5f4cda6c7952e04873a758f0083ec8ba5
Shumba Ashley <ashleytshumba@gmail.com>
2022-07-06 16:34:50 +0200

    3-Tier

--- lcd/hello_world.py
+++ lcd/hello_world.py
@@ -3,4 +3,5 @@ from time import *
 
 mylcd = I2C_LCD_driver.lcd()
 
-mylcd.lcd_display_string("Hello World!", 1)
+mylcd.lcd_display_string("Three Tier", 1)
+mylcd.lcd_display_string("Security System", 2)
